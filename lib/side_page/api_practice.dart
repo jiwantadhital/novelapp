649a6f692f4836a19f422dc97e2ed0a4d149d9a8
Akash <aakashtha49@gmail.com>
2023-06-29 20:46:35 +0545

    list api

--- lib/side_page/api_practice.dart
+++ lib/side_page/api_practice.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:novelapp/side_page/model.dart';
+import 'package:http/http.dart' as http;
 
 class ApiPractice extends StatefulWidget {
   const ApiPractice({super.key});
@@ -12,36 +12,109 @@ class ApiPractice extends StatefulWidget {
 }
 
 class _ApiPracticeState extends State<ApiPractice> {
-  late Future<Album> futureData;
 
-  Future<Album> getApiData()async{
-    var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
-    if(response.statusCode == 200){
-      return Album.fromJson(jsonDecode(response.body));
-    }
-    else{
-      throw Exception("Error");
-    }
+Future<List<AlbumList>> getData()async{
+  var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/albums"));
+  if(response.statusCode ==200){
+    return List<AlbumList>.from(jsonDecode(response.body.toString()).map((e)=>AlbumList.fromJson(e)));
   }
+  else{
+    throw Exception("Error");
+  }
+}
 
-  @override
-  void initState() {
-    futureData = getApiData();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder<Album>(
-        future: futureData,
+      body: FutureBuilder<List<AlbumList>>(
+        future: getData(),
         builder: (context,snapshot){
+        return ListView.builder(
+        itemCount: snapshot.data!.length,
+        itemBuilder: (context,index){
           if(snapshot.hasData){
-           return Center(child: Text(snapshot.data!.title.toString()));
+            return Container(
+              margin: EdgeInsets.all(20),
+              height: 20,
+              child: Text(snapshot.data![index].title.toString()));
           }
-          return CircularProgressIndicator();
+          return Center(child: CircularProgressIndicator(),);
+      });
       })
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:novelapp/side_page/model.dart';
+// import 'package:http/http.dart' as http;
+
+// class ApiPractice extends StatefulWidget {
+//   const ApiPractice({super.key});
+
+//   @override
+//   State<ApiPractice> createState() => _ApiPracticeState();
+// }
+
+// class _ApiPracticeState extends State<ApiPractice> {
+//   late Future<Album> album;
+
+// Future<Album> getData()async{
+// var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
+// if(response.statusCode == 200){
+//   return Album.fromJson(jsonDecode(response.body));
+// }
+// else{
+//   throw Exception("Error");
+// }
+// }
+
+// @override
+//   void initState() {
+//     album = getData();
+//     super.initState();
+//   }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Scaffold(
+//       appBar: AppBar(),
+//       body: FutureBuilder<Album>(
+//         future: getData(),
+//         builder: (context,snapshot){
+//           if(snapshot.hasData){
+//             return Center(child: Text(snapshot.data!.title.toString()));
+//           }
+//           return Center(child: CircularProgressIndicator());
+//       })
+//     );
+//   }
+// }
--- lib/side_page/model.dart
+++ lib/side_page/model.dart
@@ -1,11 +1,11 @@
-class Album {
+class AlbumList {
   int? userId;
   int? id;
   String? title;
 
-  Album({this.userId, this.id, this.title});
+  AlbumList({this.userId, this.id, this.title});
 
-  Album.fromJson(Map<String, dynamic> json) {
+  AlbumList.fromJson(Map<String, dynamic> json) {
     userId = json['userId'];
     id = json['id'];
     title = json['title'];
@@ -19,3 +19,39 @@ class Album {
     return data;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Album {
+//   int? userId;
+//   int? id;
+//   String? title;
+
+//   Album({this.userId, this.id, this.title});
+
+//   Album.fromJson(Map<String, dynamic> json) {
+//     userId = json['userId'];
+//     id = json['id'];
+//     title = json['title'];
+//   }
+
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['userId'] = this.userId;
+//     data['id'] = this.id;
+//     data['title'] = this.title;
+//     return data;
+//   }
+// }
